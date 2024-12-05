9a38310af92d6979eef63352a9276e96a5ab53a2
Revo Nando <rnganteng98@gmail.com>
2024-12-05 13:52:52 +0700

    feat: selesai belajar loop versi For

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-// console.log("sebelum pembanding");
-// if (1 + 1 === 2) {
-//     console.log("dalam pembanding");
+// // console.log("sebelum pembanding");
+// // if (1 + 1 === 2) {
+// //     console.log("dalam pembanding");
+// // }
+// // console.log("setelah pembanding");
+
+// let angka = Math.random();
+// console.log(angka);
+
+// if (angka >= 0.5) {
+//   console.log("Angka lebih besar sama dengan 0.5");
 // }
-// console.log("setelah pembanding");
 
-let angka = Math.random();
-console.log(angka);
+// if (angka <= 0.5) {
+//   console.log("Angka lebih kecil sama dengan 0.5");
+// }
 
-if (angka >= 0.5) {
-  console.log("Angka lebih besar sama dengan 0.5");
-}
+// let age = 8;
+// if (age === 18) {
+//   console.log("Boleh.... Boleh Masuk");
+// } else if (age === 15) {
+//   console.log("Perlu dibimbing ortu ya.");
+// } else if (age === 8) {
+//   console.log("Udah pergi ke dokter gigi?");
+// }
 
-if (angka <= 0.5) {
-  console.log("Angka lebih kecil sama dengan 0.5");
-}
+// const iniHari = "Senin";
 
-let age = 8;
-if (age === 18) {
-  console.log("Boleh.... Boleh Masuk");
-} else if (age === 15) {
-  console.log("Perlu dibimbing ortu ya.");
-} else if (age === 8) {
-  console.log("Udah pergi ke dokter gigi?");
-}
-
-const iniHari = "Senin";
-
-if (iniHari === "Senin") {
-  console.log("Selamat kerja dengan tersiksa kawan");
-} else if (iniHari === "Sabtu") {
-  console.log("Semoga jangan cepat berlalu");
-}
+// if (iniHari === "Senin") {
+//   console.log("Selamat kerja dengan tersiksa kawan");
+// } else if (iniHari === "Sabtu") {
+//   console.log("Semoga jangan cepat berlalu");
+// }
 
 // const nilai = prompt("Isikan nilai : ");
 // console.log("Nilai yang kamu input adalah " + nilai);
@@ -49,7 +49,7 @@ if (iniHari === "Senin") {
 
 // const password = prompt("Buat Passwordmu : ");
 
-//password harus 6 karakter
+// // password harus 6 karakter
 // if (password.length >= 6) {
 //   if (password.indexOf(" ") === -1) {
 //     console.log("Password VALID");
@@ -77,33 +77,79 @@ if (iniHari === "Senin") {
 //   console.log('Gak boleh akses bro, maap')
 // }
 
-let hari = prompt('isi hari ke'); // Misalnya hari ini adalah hari ke-3
+// let hari = prompt('isi hari ke'); // Misalnya hari ini adalah hari ke-3
 
-switch (hari) {
-    case 1:
-        console.log("Senin");
-        break;
-    case 2:
-        console.log("Selasa");
-        break;
-    case 3:
-        console.log("Rabu");
-        break;
-    case 4:
-        console.log("Kamis");
-        break;
-    case 5:
-        console.log("Jumat");
-        break;
-    case 6:
-        console.log("Sabtu");
-        break;
-    case 7:
-        console.log("Minggu");
-        break;
-    default:
-        console.log("Input hari tidak valid");
+// switch (hari) {
+//     case 1:
+//         console.log("Senin");
+//         break;
+//     case 2:
+//         console.log("Selasa");
+//         break;
+//     case 3:
+//         console.log("Rabu");
+//         break;
+//     case 4:
+//         console.log("Kamis");
+//         break;
+//     case 5:
+//         console.log("Jumat");
+//         break;
+//     case 6:
+//         console.log("Sabtu");
+//         break;
+//     case 7:
+//         console.log("Minggu");
+//         break;
+//     default:
+//         console.log("Input hari tidak valid");
+// }
+
+// // array
+// let warna = ['hijau', 'kuning', 'biru', 'kelabu'];
+
+// //object
+
+
+// //looping
+
+// for (let i = 0; i <= 20; i++) {
+//   console.log('Menuliskan angka '+i);
+// }
+
+
+const animals = ['cat', 'deer', 'hog', 'anjing', 'babi'];
+
+for (let i = 0; i <animals.length; i++) {
+  console.log('hewan ke-',i,' adalah '+ animals[i]);
 }
 
-// array
-let warna = ['hijau', 'kuning', 'biru', 'kelabu'];
+let asu = animals[3];
+for (let i = 0; i <= 5; i++) {
+  console.log("Luaran ke-",i);
+  for (let j = 0; j < asu.length; j++) {
+    console.log('     Dalaman: ', asu[j]);
+  }
+}
+
+let pilihan = 'abcd'
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i}. Soal nomor ${i}`);
+  for (let j = 0; j < pilihan.length; j++) {
+    console.log(`      ${pilihan[j]}. Pilihan jawaban`);
+  }
+}
+
+const studentrow = [
+  ['olivia', 'liam', 'noah'],
+  ['amelia', 'oliver', 'ava'],
+  ['sopia', 'mateo', 'isabela']
+];
+
+for (let i = 0; i < studentrow.length; i++) {
+  const row = studentrow[i];
+  console.log(`seat row #${i+1}`);
+  for (let j = 0; j < row.length; j++) {
+    console.log(`     ${row[j]}`);
+  }
+}
